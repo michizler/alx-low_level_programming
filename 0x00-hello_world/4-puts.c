d6ac0608043981abd9c47f86b3a7b14fe62f32af
Bright Uzosike <brightuzosike@gmail.com>
2022-06-02 11:19:36 -0700

    Tasks 4-6 done!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: Print a string using puts function
+ *
+ * If Return is 0, then success.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: Printing to standard output using printf
+ *
+ * If return 0, then success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
