fbeeee4f3ea3ac3fd41f0d896548c891d21f6718
Bright Uzosike <brightuzosike@gmail.com>
2022-06-07 15:41:16 -0700

    Fixed Tasks 3&4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,21 +1,18 @@
 /**
  * _isalpha - Return type int
- * args*: int c
+ * @c: integer parameter
+ * Return: Always integer value
  *
  * Description: checks for alphabetical characters
  *
  */
 
-
-
 int _isalpha(int c)
-
 {
 	int isalpha(int c);
-	
+
 	if (isalpha(c))
 		return (1);
 	else
 		return (0);
-
 }
