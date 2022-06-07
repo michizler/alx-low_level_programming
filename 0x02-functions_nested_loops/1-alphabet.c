393ac0921c43acbf6bc2a30660e16ca97188f51f
Bright Uzosike <brightuzosike@gmail.com>
2022-06-07 10:09:15 -0700

    Task 1 done!

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description - Prints lowercase alphabets
+ * Return: Always 0 (Sucess)
+ */
+
+void print_alphabet(void)
+{
+	int firstletter, i;
+
+	for (firstletter = 97, i = 1; i < 27; i++, firstletter++)
+		putchar(firstletter);
+
+}
+
+int main(void)
+{
+	print_alphabet();
+
+	putchar(10);
+	return (0);
+}
