ec117ce0dde9da2777a39f955f7972340a5c0cce
Bright Uzosike <brightuzosike@gmail.com>
2022-06-07 08:59:17 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar\n";
+	putchar(c[9]);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A project to test knowledge of functions and nested loops in C through different tasks.
