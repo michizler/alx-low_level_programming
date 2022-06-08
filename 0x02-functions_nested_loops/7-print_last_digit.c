3dc0573eac5dfe76d715dc779c50bd76e5b2489b
Bright Uzosike <brightuzosike@gmail.com>
2022-06-07 17:34:21 -0700

    Completed task 7!

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+/**
+ * print_last_digit - return last digit
+ * @n: To ascii character
+ * Return: Always int
+ *
+ * Description: print last digit using putchar().
+ *
+ */
+
+int print_last_digit(int n)
+{
+	int ret;
+
+	ret = n % 10;
+	return (ret);
+
+}
