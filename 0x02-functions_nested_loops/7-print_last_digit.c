18657b9435c0b1be8a6c5321e8d27b2de18b45b3
Bright Uzosike <brightuzosike@gmail.com>
2022-06-07 18:01:00 -0700

    Completed task 7!

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,29 @@ int print_last_digit(int n)
 {
 	int ret;
 
+	int _putchar(int c);
+
 	ret = n % 10;
+	if (ret == 0)
+		_putchar(48);
+	else if (ret == 1)
+		_putchar(49);
+	else if (ret == 2)
+		_putchar(50);
+	else if (ret == 3)
+		_putchar(51);
+	else if (ret == 4)
+		_putchar(52);
+	else if (ret == 5)
+		_putchar(53);
+	else if (ret == 6)
+		_putchar(54);
+	else if (ret == 7)
+		_putchar(55);
+	else if (ret == 8)
+		_putchar(56);
+	else
+		_putchar(57);
 	return (ret);
 
 }
