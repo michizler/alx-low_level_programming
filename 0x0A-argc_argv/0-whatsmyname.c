cbff34d1423b71f3cf722e17e77354875339c4bd
Bright Uzosike <brightuzosike@gmail.com>
2022-06-24 15:34:50 -0700

    Update 0-whatsmyname.c & README.md

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,9 @@
  * Return: Always 0 for success.
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return(0);
+	return (0);
 }
