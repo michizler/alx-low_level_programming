0a58cae251b7accf6cc17c3a9faf3f0309130738
Bright Uzosike <brightuzosike@gmail.com>
2022-06-24 14:11:25 -0700

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef _FUNCTIONS_H
+#define _FUNCTIONS_H
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +40,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
