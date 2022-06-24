6ed63bd8ef8b01e6936cc34f84472e4404232b44
Bright Uzosike <brightuzosike@gmail.com>
2022-06-24 15:13:21 -0700

    Task 0 done!

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the executable c program even
+ * when name is changed
+ * @argc: Number of command line arguments
+ * @argv: Array of character arguments
+ * Return: Always 0 for success.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return(0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,8 @@
+# Argc and Argv[]
+
+Playing with the arguments of the main() function in evry C program.
+
+*int main(int argc, char *argv[])*
+*{*
+	*return(0);*
+*}*
