33b96380946b9a1bc6642f4a49c726f801cbdd62
Bright Uzosike <brightuzosike@gmail.com>
2022-06-24 15:30:34 -0700

    Update 0-whatsmyname & README.md

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,7 +8,7 @@
  * Return: Always 0 for success.
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,8 +1,3 @@
 # Argc and Argv[]
 
 Playing with the arguments of the main() function in evry C program.
-
-*int main(int argc, char *argv[])*
-*{*
-	*return(0);*
-*}*
