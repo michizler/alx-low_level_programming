7793490b72eb48812928a07f159d57e34e999dc1
Bright Uzosike <brightuzosike@gmail.com>
2022-06-27 15:35:30 -0700

    0x0B task 0 done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,16 +14,10 @@ char *create_array(unsigned int size, char c)
 
 	unsigned int i;
 
-	if (size == 0)
-		return (NULL);
-
 	str_array = (char *)malloc(size * sizeof(char));
 
 	for (i = 0; i < size; i++)
 		str_array[i] = c;
 
-	if (str_array == NULL)
-		return (NULL);
-	else
-		return (str_array);
+	return (str_array);
 }
