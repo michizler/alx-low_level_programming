c509a2f96a348ccae46551c8fd4a97d1a685aa6d
Bright Uzosike <brightuzosike@gmail.com>
2022-06-27 15:27:38 -0700

    0x0B task 0 done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * create_array - Returns an array of char of size "size" initialized
+ * with "c"
+ * @size: size of the char array
+ * @c: character for initialization
+ * Return: Character pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str_array;
+
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	str_array = (char *)malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+		str_array[i] = c;
+
+	return (str_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+# Dynamic Allocation
+
+Hands-on practice on mannually reserving memory for arrays.
+
+- Done on Ubuntu 20.04LTS
+- Using *malloc*and *free* functions
+- Holberton Betty coding style and doc style applied.
