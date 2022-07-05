e756f9e4560ab5b23ab33e36d6afa81c2cf9578f
Bright Uzosike <brightuzosike@gmail.com>
2022-07-05 00:05:27 -0700

    Task 0 done!

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+# 0x0E- Strutures and Typedef
+
+Hand-on practice on structs and user-defined data types
+
+- Compiled on Ubuntu 20.04 LTS
+- Betty coding and documentation style implemented
+- Project source:- Holberton
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - attributes of a domestic dog
+ * @name: first attribute
+ * @age: second attribute
+ * @owner: third attribute
+ */
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+#endif
